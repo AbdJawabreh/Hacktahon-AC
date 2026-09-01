8e679b475d34b48e43e4d67210fb8188be0667bb
Abd Jawabreh <rabd79557@gmail.comh>
2026-09-01 11:40:03 +0300

    hi

---
+++ home.js
@@ -0,0 +1,18 @@
+const music = document.getElementById("music");
+const musicBtn = document.getElementById("musicBtn");
+
+if (music && musicBtn) {
+
+    musicBtn.addEventListener("click", () => {
+
+        if (music.paused) {
+            music.play();
+            musicBtn.textContent = "⏸ Pause Music";
+        } else {
+            music.pause();
+            musicBtn.textContent = "Play Music"; 
+        }
+
+    });
+
+}
