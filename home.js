238a5eb2d12ed30f537b9273ef924ac94c9c16a8
Alihashlamon-eng <ahashlamoun4444@gmail.com>
2026-09-01 13:36:32 +0300

    first commit

--- home.js
+++ home.js
@@ -16,3 +16,29 @@ if (music && musicBtn) {
     });
 
 }
+const cart = document.querySelector("#cart");
+const popup = document.querySelector("#popup");
+const popupX = document.querySelector("#popup-X");
+
+
+cart.onclick = () => {
+    popup.style.display = "flex";
+}
+popupX.onclick = () => {
+    popup.style.display = "none";
+}
+
+
+
+
+const addcart = document.querySelectorAll(".card-btn");
+const counter = document.querySelector("#counter");
+const cartSec = document.querySelector("#cartSec");
+var i = 1;
+
+addcart.forEach((add) => {
+
+    add.addEventListener("click", () => {
+        counter.textContent =i++ ;
+    });
+})
